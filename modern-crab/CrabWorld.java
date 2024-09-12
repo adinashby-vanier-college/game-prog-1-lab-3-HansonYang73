a776a7c409b28fbacd00288c74aa804a77e154dc
Hanson Yang <6323527@edu.vaniercollege.qc.ca>
2024-09-12 14:20:44 -0400

    Moddified game

--- modern-crab/CrabWorld.java
+++ modern-crab/CrabWorld.java
@@ -17,5 +17,36 @@ public class CrabWorld extends World
     public CrabWorld()
     {
         super(560, 560, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab =  new  Crab();
+        addObject(crab, 292, 292);
+        Worm worm =  new  Worm();
+        addObject(worm, 150, 108);
+        Worm worm2 =  new  Worm();
+        addObject(worm2, 326, 120);
+        Worm worm3 =  new  Worm();
+        addObject(worm3, 472, 222);
+        Worm worm4 =  new  Worm();
+        addObject(worm4, 383, 178);
+        Worm worm5 =  new  Worm();
+        addObject(worm5, 175, 164);
+        Worm worm6 =  new  Worm();
+        addObject(worm6, 230, 120);
+        Worm worm7 =  new  Worm();
+        addObject(worm7, 444, 379);
+        Worm worm8 =  new  Worm();
+        addObject(worm8, 265, 380);
+        Worm worm9 =  new  Worm();
+        addObject(worm9, 152, 251);
+        Worm worm10 =  new  Worm();
+        addObject(worm10, 103, 372);
     }
 }
---
+++ modern-crab/Lobster.java
@@ -0,0 +1,81 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Lobster extends Actor
+{
+
+    /**
+     * 
+     */
+    public Lobster()
+    {
+        turn(Greenfoot.getRandomNumber(360));
+    }
+
+    /**
+     * Act - do whatever the Lobster wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        moveAround();
+        eat();
+        spawnWorm();
+        zombie();
+    }
+
+    /**
+     * 
+     */
+    public void moveAround()
+    {
+        move(4);
+        if (Greenfoot.getRandomNumber(10) == 1) {
+            turn(Greenfoot.getRandomNumber(90) - 45);
+        }
+        if (isAtEdge()) {
+            turn(180);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void eat()
+    {
+        Actor crab = getOneIntersectingObject(Crab.class);
+        if (crab != null) {
+            World world = getWorld();
+            world.removeObject(crab);
+            Greenfoot.playSound("gameover2.wav");
+        }
+    }
+
+    /**
+     * 
+     */
+    public void zombie()
+    {
+        Actor worm = getOneIntersectingObject(Worm.class);
+        if (worm != null) {
+            World world = getWorld();
+            world.addObject( new  Lobster(), worm.getX(), worm.getY());
+            world.removeObject(worm);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void spawnWorm()
+    {
+        if (Greenfoot.getRandomNumber(1000) == 1) {
+            World world = getWorld();
+            world.addObject( new  Worm(), Greenfoot.getRandomNumber(560), Greenfoot.getRandomNumber(560));
+        }
+    }
+}
